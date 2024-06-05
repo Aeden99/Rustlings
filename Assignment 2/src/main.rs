c773f4eb7be552abf0c7d2f54293dae92f13419b
Devansh Bhardwaj <in.devansh.bhardwaj@gmail.com>
2024-06-05 23:25:11 +0530

    Change

--- Assignment 2/src/main.rs
+++ Assignment 2/src/main.rs
@@ -2,9 +2,6 @@
 //given on hyper crate's github documentation.
 //Due to insufficient knowledge of alternatives of some data types and functions here,
 //as well as their crucialness, I have not made any changes to some parts of their code.
-//Rest assured I have tried my best to understand the code and not copy paste here.
-
-#![deny(warnings)]
 
 use http_body_util::BodyExt;
 use std::cell::Cell;
@@ -54,31 +51,111 @@ impl HttpBody for Body {
 fn main(){
     pretty_env_logger::init();
     let server_http1 = thread::spawn(move || {
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
+        let rntm = tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .enable_time()
             .build()
-            .expect("build runtime");
+            .expect("runtime could not be built");
         let local = tokio::task::LocalSet::new();
-        local.block_on(&rt, http1_server()).unwrap();
+        local.block_on(&rntm, http1_server()).unwrap();
     });
 
     let client_http1 = thread::spawn(move || {
-        // Configure a runtime for the client that runs everything on the current thread
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
+        let rntm = tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .enable_time()
             .build()
-            .expect("build runtime");
-
-        // Combine it with a `LocalSet,  which means it can spawn !Send futures...
+            .expect("runtime could not be built");
         let local = tokio::task::LocalSet::new();
         local
             .block_on(
-                &rt,
-                http1_client("http://localhost:3001".parse::<hyper::Uri>().unwrap()),
+                &rntm,
+                http1_client("http://127.0.0.1:7878".parse::<hyper::Uri>().unwrap()),
+                //Using localhost here instead of this IpV4 address could have serve as an enum
+                //of an IpV4 and and IpV6 (::1) address
             )
             .unwrap();
     });
 
     server_http1.join().unwrap();
     client_http1.join().unwrap();
+}
+async fn http1_server() -> Result<(), Box<dyn std::error::Error>> {
+
+    let listener = TcpListener::bind(([127,0,0,1],7878)).await?;
+    let counter = Rc::new(Cell::new(0));
+
+    loop {
+        let (stream, socket) = listener.accept().await?;
+
+        let io = IOTypeNotSend::new(TokioIo::new(stream));
+
+        let cnt = counter.clone();
+        //This is a static type allocation allowing parallel connections 
+
+        let service = service_fn(move |_| {
+            let prev = cnt.get();
+            cnt.set(prev + 1);
+            let value = cnt.get();
+            async move { Ok::<_, Error>(Response::new(Body::from(format!("Request #{}", value)))) }
+        });
+
+        tokio::task::spawn_local(async move {
+            if let Err(err) = hyper::server::conn::http1::Builder::new()
+                .serve_connection(io, service)
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
+
+
+struct IOTypeNotSend {
+    _marker: PhantomData<*const ()>,
+    stream: TokioIo<TcpStream>,
+}
+
+impl IOTypeNotSend {
+    fn new(stream: TokioIo<TcpStream>) -> Self {
+        Self {
+            _marker: PhantomData,
+            stream,
+        }
+    }
+}
+
+impl hyper::rt::Write for IOTypeNotSend {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        Pin::new(&mut self.stream).poll_write(cx, buf)
+    }
+
+    fn poll_flush(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        Pin::new(&mut self.stream).poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<() , std::io::Error>> {
+        Pin::new(&mut self.stream).poll_shutdown(cx)
+    }
+}
+
+impl hyper::rt::Read for IOTypeNotSend {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: hyper::rt::ReadBufCursor<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        Pin::new(&mut self.stream).poll_read(cx, buf)
+    }
 }
